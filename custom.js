5f4d4851d070bbb2057eeb8934ffcd293b150061
Ryan Brunt <ryanbrunt@savvysherpa.com>
2014-09-26 13:40:27 -0500

    auto hide input

--- custom.js
+++ custom.js
@@ -47,7 +47,7 @@
  * @class customjs
  * @static
  */
-
+$('.input').slideToggle()
 $([IPython.events]).on("app_initialized.NotebookApp", function () {
         $('div#header').hide();
             $('div#maintoolbar').hide();
