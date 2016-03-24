38059e02802a9174df857321822c3fb3ada92992
Ryan Brunt <rjhbrunt@gmail.com>
2016-03-24 12:21:51 -0400

    auto push

--- autoPush.sh
+++ autoPush.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-ipython nbconvert index.ipynb --to html --template .main.tpl
-ipython nbconvert about.ipynb --to html --template .main.tpl
-ipython nbconvert research.ipynb --to html --template .main.tpl
+jupyter nbconvert index.ipynb --to html --template .main.tpl
+jupyter nbconvert about.ipynb --to html --template .main.tpl
+jupyter nbconvert research.ipynb --to html --template .main.tpl
 git add --all
 git commit -m "auto push"
 git push origin master
